5d5d433261dc350a5172f32f43705b79850d1618
Eva <as45986@gmail.com>
2024-08-10 15:15:53 +0800

    fix:cors for all

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 //   methods: ["GET", "POST", "PUT", "DELETE"],
 //   credentials: true
 // }));
-app.options('*', cors({
-  origin: allowedOrigins,
+app.use(cors({
+  origin: '*',
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
