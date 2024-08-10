c2d792c8f32aefc63eacfd1dc2b63f9da206551b
Eva <as45986@gmail.com>
2024-08-10 15:19:34 +0800

    fix:index

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import './config/db.js';
 import { Router } from './routes/routes.js';
+
+// 從名為 config.env 的文件中加載環境變數到 process.env 中。這個文件通常包含敏感信息，例如 API 金鑰、數據庫 URL , port 等。
+dotenv.config({ path: './config.env' })
 const allowedOrigins = process.env.CORS_ORIGIN.split(',');
 console.log(allowedOrigins);
 const app = express();
 app.use(express.json());
+
 // app.use(cors({
 //   origin: allowedOrigins,
 //   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -17,8 +21,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-// 從名為 config.env 的文件中加載環境變數到 process.env 中。這個文件通常包含敏感信息，例如 API 金鑰、數據庫 URL , port 等。
-dotenv.config({ path: './config.env' })
+
 
 //  Express 中的一個方法，用來將Middleware綁定到應用程序
 // '/contactmsyt': 這是基礎路徑（base route）。
